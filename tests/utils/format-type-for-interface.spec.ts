16fc6fb4ea28f2aa0e47fc4fc493cba253720c82
Guilherme Nallon <guilhermeleonardonallon@gmail.com>
2024-10-23 20:49:57 -0300

    test: formatTypeForInterface

---
+++ tests/utils/format-type-for-interface.spec.ts
@@ -0,0 +1,33 @@
+import { formatTypeForInterface } from '../../src/utils'
+
+describe('Format Type For Interface', () => {
+  it('Should return "string" when passed "string"', () => {
+    const result = formatTypeForInterface('string');
+
+    expect(result).toStrictEqual('string');
+  });
+
+  it('Should return "number" when passed "number"', () => {
+    const result = formatTypeForInterface('number');
+
+    expect(result).toStrictEqual('number');
+  });
+
+  it('Should return "date" when passed "date"', () => {
+    const result = formatTypeForInterface('date');
+
+    expect(result).toStrictEqual('Date');
+  });
+
+  it('Should return "uuid" when passed "uuid"', () => {
+    const result = formatTypeForInterface('uuid');
+
+    expect(result).toStrictEqual('string');
+  });
+
+  it('Should return "boolean" when passed "boolean"', () => {
+    const result = formatTypeForInterface('boolean');
+
+    expect(result).toStrictEqual('boolean');
+  });
+})
