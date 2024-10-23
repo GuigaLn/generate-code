0ccd16af674c5a7778d53a4b735f87c8f370bb9e
Guilherme Nallon <guilhermeleonardonallon@gmail.com>
2024-10-23 20:49:36 -0300

    feat: formatTypeForInterface

---
+++ src/utils/format-type-for-interface.ts
@@ -0,0 +1,12 @@
+import { IType } from 'interfaces';
+
+export function formatTypeForInterface(type: IType) {
+  switch(type) {
+    case 'uuid':
+      return 'string'
+    case 'date':
+      return 'Date'
+    default:
+      return type;
+  }
+}
